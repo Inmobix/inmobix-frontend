36ebd78490e5ec2c073cf5ab804a55bde2d07dcd
Andrés Gómez <afgomezv@ufpso.edu.co>
2025-09-28 17:32:18 -0500

    INF-17 AndresGomez: Se creó el DashboardComponent con plantilla responsive y links preparados para rutas. Además se creó un guard para solo si esta logeado, permita entrar al dashboard.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }, // Falta crear componente 404
 ];
