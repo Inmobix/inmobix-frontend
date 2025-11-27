5fff248265102a9adbfd996a2a5581f9c11a3bc1
JordyPradaYanes <jdpraday@ufpso.edu.co>
2025-11-27 02:40:33 -0500

    INF-32 correccion de errores

---
+++ src/app/components/dashboard/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SidebarComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
